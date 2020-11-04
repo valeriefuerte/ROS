093c40cad34e39312489dfeacb1c1f7a162c9484
valeria <leradopira@mail.ru>
2020-11-04 14:02:35 +0300

    added spawner

---
+++ l1/src/lab1/spawner.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python
+
+import rospy
+from turtlesim.srv import Spawn
+
+
+rospy.init_node('turtle2_spawner_node')
+
+rospy.wait_for_service('/spawn')
+spawn_func = rospy.ServiceProxy('/spawn', Spawn)
+
+turtle_name = rospy.get_param('~turtle2')
+turtle2 = spawn_func(1.0, 1.0, 1.0, turtle_name)
