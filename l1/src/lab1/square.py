54d3d337e3c9a445f12a169680fadafd5f5b3cd5
valeria <leradopira@mail.ru>
2020-11-04 14:21:13 +0300

    edited sqare example

--- l1/src/lab1/square.py
+++ l1/src/lab1/square.py
@@ -2,32 +2,35 @@
 
 import rospy
 from geometry_msgs.msg import Twist
-import math
 import time
+import math
 
-def move_forward(pub, msg, vel):
-	msg.linear.x = vel
-	msg.angular.z = 0.0
-	pub.publish(msg)
+def move_forward(pub, msg):
+    msg.linear.x = 4.0
+    msg.angular.z = 0.0
+    pub.publish(msg)
 
 def move_rotate(pub, msg):
-	msg.linear.x = 0
-	msg.angular.z= math.pi/2.0
-	pub.publish(msg)
+    msg.linear.x = 0.0
+    msg.angular.z = math.pi/2
+    pub.publish(msg)
 
 rospy.init_node('name_by_default')
-pub1 = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size = 1)
 
-vel = float(rospy.get_param('speed')
-
+pub1 = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=1)
+pub2 = rospy.Publisher('leonardo/cmd_vel', Twist, queue_size=1)
 msg = Twist()
-time.sleep(1)
 
-r = rospy.Rate(0.5) #Hz 
+r = rospy.Rate(0.5) #Hz
 
 while not rospy.is_shutdown():
-	move_forward(pub1, msg, vel)
-	time.sleep(1)
-	move_rotate(pub1, msg)
-	r.sleep()
+    move_forward(pub1, msg)
+    move_forward(pub2, msg)
+
+    time.sleep(1)
+
+    move_rotate(pub1, msg)
+    move_rotate(pub2, msg)
+
+    r.sleep()
 
